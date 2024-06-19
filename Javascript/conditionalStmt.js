752ef0bf7b26af65066cbdb80a83d0f43e0fded5
Hemarajsgithub <hemaraj2708@gmail.com>
2024-06-19 13:13:18 +0530

    Looping Statement

--- Javascript/conditionalStmt.js
+++ Javascript/conditionalStmt.js
@@ -38,20 +38,90 @@ else{
 */
 
 
-let hours = -1;
+// let hours = -1;
 
-if( hours>=1 && hours<=6 ){
-    console.log( "Early Morning" );
-}
-else if(  hours>=7 && hours<=12 ){
-    console.log( "Good Morning" );
-}
-else if(  hours>=13 && hours<=18 ){
-    console.log( "Good Noon" );
-}
-else if(  hours>=19 && hours<=24 ){
-    console.log( "Good Night" );
-}
-else{
-    console.log( "It is not a valid hour" );
-}
+// if( hours>=1 && hours<=6 ){
+//     console.log( "Early Morning" );
+// }
+// else if(  hours>=7 && hours<=12 ){
+//     console.log( "Good Morning" );
+// }
+// else if(  hours>=13 && hours<=18 ){
+//     console.log( "Good Noon" );
+// }
+// else if(  hours>=19 && hours<=24 ){
+//     console.log( "Good Night" );
+// }
+// else{
+//     console.log( "It is not a valid hour" );
+// }
+
+// let mark = 65;
+
+// if( mark>=90 && mark<=100 ){
+//     console.log( "S Grade" );
+// }
+// else if(mark>=80 && mark<=89){
+//     console.log( "A Grade" );
+// }
+// else if(mark>=60 && mark<=79){
+//     console.log( "B Grade" );
+// }
+// else if(mark>=40 && mark<=59){
+//     console.log( "C Grade" );
+// }
+// else if(mark>=1 && mark<=39){
+//     console.log( "Arrear" );
+// }
+// else{
+//     console.log( "It is not consider as a mark" );
+// }
+
+
+//Switch Statement
+
+/*
+    switch( expression ){
+        case expression : statement;
+        break;
+        case expression : statement;
+        break;
+        case expression : statement;
+        break;
+        default : statement;
+        break;
+    }
+*/
+
+// let trafficLight = "over speed"
+
+// switch(trafficLight){
+//     case "red" : console.log( "Stop" );
+//     break;
+//     case "yellow" : console.log( "Wait" );
+//     break;
+//     case "green" : console.log( "You can go" );
+//     break;
+//     default : console.log( "Pay Fine"  );
+//     break;
+// }
+
+
+// //Ternary Operator
+
+// // Syntax : condition ? "Statement1" : "Statement2";
+// let uAge = 17;
+
+// let output;
+// uAge>=18 ? output="Eligible" : output = "Not Eligible";
+
+// console.log( output );
+
+// let data = null;
+
+// data !=null ? console.log("User Data") : console.log( "Loading..." );
+
+
+
+
+
---
+++ Javascript/loopingStmt.js
@@ -0,0 +1,111 @@
+//Looping Statement
+
+//for loop
+//while loop
+//do-while loop
+
+//for loop
+
+/*
+
+    for( initialization; condition; counter){
+        statement;
+    }
+
+*/
+
+
+// for( let i = 0; i<=100; i+=5 ){  // i = i + 3 = 0 + 3 = 3
+//     console.log( i );
+// }
+
+// let tableNum = 5;
+
+// for( let i=1; i<=20; i++ ){
+//     console.log( `${tableNum}*${i}=${tableNum*i}`  );
+// }
+
+// for (let i = 10; i>=1; i--) {
+//     console.log( i );
+// }
+
+
+//While Loop
+
+/* 
+    initialization;
+    while(condition){
+        Statement...
+        counter++;
+    }
+*/
+
+// let val = 10;
+
+// while( val>=1 ){
+//     console.log( val );
+//     val--;
+// }
+
+//-------------------------------------------------
+// let num = 4352;
+// let sum = 0;
+// // 4 + 3 + 5 + 2 = 14
+
+// // console.log( num%10  );
+
+// let last = num%10; // 2
+// num = parseInt (num/10) // 435
+// sum = sum + last // 0 + 2 = 2
+
+// console.log( num, sum );
+
+// last = num%10 // 5
+// num = parseInt( num/10 ) //43
+// sum = sum + last // 2 + 5 = 7
+
+//--------------------------------------------------
+
+
+// let num = 234; 
+// let sum = 0 //22
+// let revVal = 0;  //7
+// while( num>0 ){
+//     let last = num%10; //4
+//     num = parseInt( num/10 ) //0
+//     revVal = revVal*10 + last
+//     sum += last // sum = 18 + 4 = 22
+// }
+
+// console.log( sum );
+// console.log( revVal );
+
+// let newNum = 40
+// do{
+//     console.log( newNum );
+//     newNum++;
+// }
+// while( newNum<30 )
+
+
+// for (let index = 1; index <= 10; index++) {
+//     // if( index == 6 ){
+//     //     break;
+//     // }
+//     if( index == 6 ){
+//         continue;
+//     }
+//     console.log( index ); 
+// }
+
+
+for( i=1; i<=10; i++ ){
+    if( i%2 == 0 ){
+        console.log( i );
+    }
+}
+for( i=1; i<=10; i++ ){
+    if( i%2 == 1 || i%2 !=0 ){
+        console.log( i );
+    }
+}
